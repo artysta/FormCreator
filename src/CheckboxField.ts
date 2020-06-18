31fa3742f5fbeef562a9e5a30d31893eb789bcaa
artysta <adrian.kurek93@gmail.com>
2020-06-18 22:22:08 +0200

    Add checkbox field

---
+++ src/CheckboxField.ts
@@ -0,0 +1,28 @@
+import { Field } from "./Field";
+import { FieldType } from "./FieldType";
+import { LabelField } from "./LabelField";
+
+export class CheckboxField implements Field {
+    name: string;
+    type: FieldType;
+    value: any;
+    lblElement: LabelField;
+    lblValue: string;
+    checkboxElement: HTMLInputElement;
+    
+    constructor(name: string, lblValue: string){
+        this.name = name;
+        this.type = FieldType.Checkbox;
+        this.checkboxElement = <HTMLInputElement> document.createElement('input');
+        this.checkboxElement.name = this.name;
+        this.lblElement = new LabelField("label", lblValue);
+    }
+    
+    render(): HTMLElement {
+        return this.checkboxElement;
+    }
+
+    getValue() {
+        return this.checkboxElement.value;
+    }
+}
