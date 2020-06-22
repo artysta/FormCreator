84b10b133d85b6fba11cc92ca024cf2274b08ab5
artysta <adrian.kurek93@gmail.com>
2020-06-22 19:51:03 +0200

    Add date field

---
+++ src/DateField.ts
@@ -0,0 +1,28 @@
+import { Field } from "./Field";
+import { FieldType } from "./FieldType";
+import { LabelField } from "./LabelField";
+
+export class DateField implements Field {
+    name: string;
+    type: FieldType;
+    value: any;
+    lblElement: LabelField;
+    lblValue: string;
+    dateElement: HTMLInputElement;
+    
+    constructor(name: string, lblValue: string){
+        this.name = name;
+        this.type = FieldType.Date;
+        this.dateElement = <HTMLInputElement> document.createElement('input');
+        this.dateElement.name = this.name;
+        this.lblElement = new LabelField("label", lblValue);
+    }
+    
+    render(): HTMLElement {
+        return this.dateElement;
+    }
+
+    getValue() {
+        return this.dateElement.value;
+    }
+}
