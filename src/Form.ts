096f9b16a265bfe391030a1b0ccd379b513e4926
artysta <adrian.kurek93@gmail.com>
2020-06-23 18:58:33 +0200

    Add form class

---
+++ src/Form.ts
@@ -0,0 +1,27 @@
+import {Field} from './Field';
+
+export class Form {
+    fields: Field[];
+    formContainer: HTMLElement;
+
+    /**
+     * @param containerId  Id of element in which the form should be placed.
+     * @param fields  The array of elements that the form should consist of.
+     */
+    constructor(containerId: string, fields: Field[]) {
+        this.formContainer = document.getElementById(containerId);
+        this.fields = fields;
+    }
+
+    render(): void {
+        this.fields.forEach(element => {
+            this.formContainer.appendChild(element.lblElement.render());
+            this.formContainer.appendChild(element.render());
+        });
+    }
+
+    getValue(): void {
+        // TODO
+    }
+
+}
