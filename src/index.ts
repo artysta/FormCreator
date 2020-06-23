5dc247b131ce70d4c551b59ef3076adf639dd7c3
artysta <adrian.kurek93@gmail.com>
2020-06-23 18:59:13 +0200

    Display simple form

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 import { InputField } from "./InputField";
+import { SelectField } from "./SelectField";
+import { TextAreaField } from "./TextAreaField";
+import { Form } from "./Form";
+import { Field } from "./Field";
+import { CheckboxField } from "./CheckboxField";
 
 let hello: string = "Hello World!</br>";
 document.body.innerHTML = hello;
 
-let field: InputField = new InputField("input", "Name:");
-document.body.appendChild(field.lblElement.render());
-document.body.appendChild(field.render());
+let inputName: InputField = new InputField("input", "Name:");
+let inputCity: InputField = new InputField("input", "City:");
+let selectLanguages: SelectField = new SelectField("select", "Favorite language:", ["Java", "Python", "C#", "TypeScript"]);
+let checkBox: CheckboxField = new CheckboxField("checkbox", "Do you like e-learning?");
+
+let array: Array<Field> = [inputName, inputCity, selectLanguages, checkBox];
+
+let form: Form = new Form("body", array);
+
+form.render();
