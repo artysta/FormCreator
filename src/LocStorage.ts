90efa97534be26bed0b211678fe42e85b5c3c94d
artysta <adrian.kurek93@gmail.com>
2020-06-28 15:31:26 +0200

    Add methods

--- src/LocStorage.ts
+++ src/LocStorage.ts
@@ -1,5 +1,15 @@
+/*
 import { DataStorage } from "./DataStorage"
 
-export LocStorage implements DataStorage {
+export class LocStorage implements DataStorage {
+    saveDocument(values: any): string {
+        throw new Error("Method not implemented.");
+    }
+    loadDocument(documentId: string) {
+        throw new Error("Method not implemented.");
+    }
+    getDocuments(): string[] {
+        throw new Error("Method not implemented.");
+    }
 
-}
+}*/
