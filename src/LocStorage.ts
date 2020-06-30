eba6393a0bc73873dbab67af476b0d1f35de503c
artysta <adrian.kurek93@gmail.com>
2020-06-30 20:43:49 +0200

    Implement code of all methods

--- src/LocStorage.ts
+++ src/LocStorage.ts
@@ -1,15 +1,33 @@
-/*
 import { DataStorage } from "./DataStorage"
 
 export class LocStorage implements DataStorage {
     saveDocument(values: any): string {
-        throw new Error("Method not implemented.");
-    }
-    loadDocument(documentId: string) {
-        throw new Error("Method not implemented.");
-    }
-    getDocuments(): string[] {
-        throw new Error("Method not implemented.");
+        let timestamp: string = Date.now().toString();
+        let jsonString: string = JSON.stringify(values);
+
+        localStorage.setItem(timestamp, jsonString);
+
+        return timestamp;
+        // throw new Error("Method not implemented.");
     }
 
-}*/
+    /**
+     * @param documentId  ID of document that should be loaded.
+     * @returns  JSON that consists of 
+     */
+    loadDocument(documentId: string): object {
+        return JSON.parse(localStorage.getItem(documentId));
+    }
+
+    /**
+     * @returns  List that consists all document IDs from local storage.
+     */
+    getDocuments(): string[] {
+        let documentIDs: string[];
+
+        for (let i = 0; i < localStorage.length; i++)
+            documentIDs.push(localStorage.key(i));
+
+        return documentIDs;
+    }
+}
