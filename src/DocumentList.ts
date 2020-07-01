430a79e86f2dea94e8b4f1751ae747b2a5cab5c5
artysta <adrian.kurek93@gmail.com>
2020-07-01 18:03:24 +0200

    Implement methods

--- src/DocumentList.ts
+++ src/DocumentList.ts
@@ -1,9 +1,54 @@
+import { LocStorage } from "./LocStorage";
+
 export class DocumentList {
-    getDocumentList() {
-        // TODO
+    documents: any[];
+
+    constructor() {
+        this.documents = new Array();
     }
 
-    render() {
-        // TODO
+    /**
+     * Load all documents from the local storage and save these to the array.
+     */
+    getDocumentList(): void {
+        let storage: LocStorage = new LocStorage();
+        let documentJSONs: string[] = storage.getDocuments();
+
+        documentJSONs.forEach(documentID => {
+            let currentDoc: any = storage.loadDocument(documentID);
+            
+            // Add documentID to current document JSON.
+            currentDoc["Document ID:"] = documentID;
+            
+            this.documents.push(currentDoc);
+        });
+    }
+
+    /**
+     * @returns  Table (HTMLElement) that contains all documents.
+     */
+    render(): HTMLElement {
+        let table = document.createElement("table");
+
+        this.documents.forEach(currentDoc => {
+            for (let key in currentDoc) {
+                console.log(key + " " + currentDoc[key]);
+
+                let tr = document.createElement("tr");
+                let tdKey = document.createElement("td");
+                let tdValue = document.createElement("td");
+        
+                tdKey.innerHTML = key;
+                tdValue.innerHTML = currentDoc[key];
+
+                tr.appendChild(tdKey);
+                tr.appendChild(tdValue);
+                table.appendChild(tr);
+            }
+        });
+
+        
+
+        return table;
     }
 }
