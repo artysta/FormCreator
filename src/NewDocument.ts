0a87118ca6e552d7ef674f26a99be06fe7a29822
artysta <adrian.kurek93@gmail.com>
2020-07-01 20:20:07 +0200

    Add new html page and script file so the new form can be created

---
+++ src/NewDocument.ts
@@ -0,0 +1,18 @@
+import { InputField } from "./InputField";
+import { SelectField } from "./SelectField";
+import { CheckboxField } from "./CheckboxField";
+import { Field } from "./Field";
+import { Form } from "./Form";
+
+console.log("Hello - New document!");
+
+let inputName: InputField = new InputField("input", "Name:");
+let inputCity: InputField = new InputField("input", "City:");
+let selectLanguages: SelectField = new SelectField("select", "Favorite language:", ["Java", "Python", "C#", "TypeScript"]);
+let checkBox: CheckboxField = new CheckboxField("checkbox", "Do you like e-learning?");
+
+let array: Array<Field> = [inputName, inputCity, selectLanguages, checkBox];
+
+let form: Form = new Form("form-container", array);
+
+form.render();
