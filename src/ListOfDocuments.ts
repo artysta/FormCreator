145c6332e3df2bf92d391e1808a5ab5e0f689ced
artysta <adrian.kurek93@gmail.com>
2020-07-06 17:21:12 +0200

    Display message if there are no documents

--- src/ListOfDocuments.ts
+++ src/ListOfDocuments.ts
@@ -3,4 +3,8 @@ import { DocumentList } from "./DocumentList";
 
 let documentList: DocumentList = new DocumentList();
 documentList.getDocumentList();
+
+if (documentList.documents.length < 1)
+    document.getElementById("header").innerHTML = "There are no documents, but you can <a href='form-list.html'>fill existing form</a>.";
+
 document.getElementById("list").appendChild(documentList.render());
